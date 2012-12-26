bfecb4ffe4c65702db46ea20d62e3782e1cacf6c
basile bouquet <bas.bouquet@gmail.com>
2012-12-26 10:57:30 -0800

    tata

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Titi::Application.config.secret_token = '2c9d7bd69bb1f759c63834cab0ebcc569056229a6f24fb64600c518a246635e4fd167b833a4aee4ce965b62c80ac366a3fc9f7af07f7494ea702affbe06abd6c'
